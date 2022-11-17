121fbe287c24dadd24040f41a839b38c48e37670
MiaktynovS <ousm9595@gmail.com>
2022-11-17 15:02:35 +0200

    Add files

---
+++ src/js/02-video.js
@@ -0,0 +1,13 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+const currentTime = 'videoplayer-current-time';
+
+
+player.on('timeupdate', throttle(currentTimeUpdate, 1000));
+function currentTimeUpdate({ seconds }) {
+  localStorage.setItem(currentTime, seconds);
+}
+player.setCurrentTime(localStorage.getItem(currentTime));
